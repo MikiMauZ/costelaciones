bfb80da4b65188e58b7007b75d5764e5a5a22df5
MikiMauZ <62695740+MikiMauZ@users.noreply.github.com>
2025-11-13 10:29:46 +0000

    Configurar base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 👇 IMPORTANTE: cambia 'costelaciones' si tu repo se llama distinto
 export default defineConfig({
   plugins: [react()],
+  base: '/costelaciones/',
 })
